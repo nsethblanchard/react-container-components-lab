8b6447d1fd1dff43401e8fb845013109daf96127
Seth Blanchard <nsethblanchard@gmail.com>
2021-06-22 14:05:37 -0400

    commit tree bb75bdb3eac1727af813c2071505b6bfe4456b54

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -4,6 +4,33 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'your-key-here';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+    + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+            .then(res => res.json())
+            .then(response => this.setState({ reviews: response.results }));
+    }
+
+
+    render() {
+
+        return (
+            <div className="latest-movie-reviews">
+                <h2>The Latest Reviews:</h2>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer;
